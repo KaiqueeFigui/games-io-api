e5045749ff44e18b675402531fe5108262b2c99d
Kaique Lucena <kaique.lucena@cerc.inf.br>
2021-11-21 04:08:54 -0300

    feat: Adding RESET_PASSWORD message and subject to Constants.java

--- src/main/java/io/games/api/gamesioapi/utils/Constants.java
+++ src/main/java/io/games/api/gamesioapi/utils/Constants.java
@@ -6,4 +6,6 @@ public class Constants {
     public static final String ROLE_ADMIN = "ADMIN";
     public static final String UPDATED_ACCOUNT_MESSAGE = "Hello %s your Games-io Account was updated at %s ";
     public static final String UPDATED_ACCOUNT_SUBJECT = "Security alert: Your Account was updated";
+    public static final String RESET_PASSWORD_SUBJECT = "Token to reset your password";
+    public static final String RESET_PASSWORD_MESSAGE = "Hello %s there's your token to reset you password: %s";
 }
