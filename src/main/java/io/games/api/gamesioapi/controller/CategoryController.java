ab86b55fb1f684210543c2c2c8190436157b7969
Kaique Lucena <kaique.lucena@cerc.inf.br>
2021-11-23 01:40:04 -0300

    feat: adding caching

--- src/main/java/io/games/api/gamesioapi/controller/CategoryController.java
+++ src/main/java/io/games/api/gamesioapi/controller/CategoryController.java
@@ -5,6 +5,8 @@ import io.games.api.gamesioapi.dto.request.PageableRequest;
 import io.games.api.gamesioapi.dto.response.CategoryResponse;
 import io.games.api.gamesioapi.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,21 +21,25 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @PostMapping
+    @CacheEvict(value = {"category-by-id", "category-by-name", "category-page"})
     public ResponseEntity<CategoryResponse> postCategory(@RequestBody @Valid CategoryRequest categoryRequest){
         return ResponseEntity.status(201).body(categoryService.postCategory(categoryRequest));
     }
 
     @GetMapping("/by-id/{id}")
+    @Cacheable("category-by-id")
     public ResponseEntity<CategoryResponse> getCategoryById(@PathVariable Integer id){
         return ResponseEntity.status(200).body(categoryService.getCategoryById(id));
     }
 
     @GetMapping("/by-name/{name}")
+    @Cacheable("category-by-name")
     public ResponseEntity<CategoryResponse> getCategoryByName(@PathVariable String name){
         return ResponseEntity.status(200).body(categoryService.getCategoryByName(name));
     }
 
     @GetMapping
+    @Cacheable("category-page")
     public ResponseEntity<Page<CategoryResponse>> getCategoryByPage(PageableRequest pageableRequest){
         return ResponseEntity.status(200).body(categoryService.getCategoryPage(pageableRequest));
     }
